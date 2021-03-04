c485c6e656ce00c14b3e85a1b3c3e37181cc3a35
William Krug <william.p.krug@gmail.com>
2021-03-04 10:36:13 -0600

    set up Redux store and sagas

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,10 @@
+/* Import Libraries */
+import { useSelector } from 'react-redux';
+
+function SearchResults() {
+  const giphyResults = useSelector((store) => store.giphyResults);
+
+  return;
+}
+
+export default SearchResults;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
+/* Import Libraries */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+/* Import App */
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function* rootSaga() {}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const giphyResults = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    giphyResults,
+  }),
+  applyMiddleware(sagaMiddleware, logger)
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
