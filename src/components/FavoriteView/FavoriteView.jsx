a47f50a21d0282d0356312c981cacedf7cf0af1b
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-04 16:43:10 -0600

    got favorites to display

--- src/components/FavoriteView/FavoriteView.jsx
+++ src/components/FavoriteView/FavoriteView.jsx
@@ -1,17 +1,39 @@
 import {useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 
 const FavoriteView = function () {
+  const dispatch = useDispatch();
 
   // get the redux state to a local variable
   const favoriteList = useSelector(store => store.favoriteGiphy);
+
+  
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  console.log('favorite list is', favoriteList);
+
+
+  const getFavorites = function () {
+
+    dispatch({
+      type: 'GET_FAVORITE'
+    }); 
+
+  }; // end getFavorites
+
 
   return (
     <div className='all_favorites'>
       <h2>Favorite GIFs</h2>
       {favoriteList.map(favorite => {
-        <div className="individ_favorite">
-          <img src={favorite.url} />
+        return (
+        <div key={favorite.id}className="individ_favorite">
+          <img src={favorite.image_url} />
         </div>
+        )
       })}
 
     </div>
--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
 
-const dragonList = [
-  'https://m.media-amazon.com/images/I/61Hj7h3wleL._AC_SL1024_.jpg',
-  'https://media.istockphoto.com/illustrations/red-dragon-head-digital-painting-illustration-id1183916666?k=6&m=1183916666&s=170667a&w=0&h=r0j8_L08luju5A-2EbFsB6CUznw5m6nfkN-cIoUiwlk=',
-  'https://i.ytimg.com/vi/gdNLiiaNOdk/maxresdefault.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Welsh_Dragon_%28Y_Ddraig_Goch%29.svg/1200px-Welsh_Dragon_%28Y_Ddraig_Goch%29.svg.png',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/330px-Smaug_par_David_Demaret.jpg',
-  'https://api.time.com/wp-content/uploads/2014/06/how-to-train-your-dragon-2-movie-still.jpg?quality=85&w=1200&h=628&crop=1',
-];
+// const dragonList = [
+//   'https://m.media-amazon.com/images/I/61Hj7h3wleL._AC_SL1024_.jpg',
+//   'https://media.istockphoto.com/illustrations/red-dragon-head-digital-painting-illustration-id1183916666?k=6&m=1183916666&s=170667a&w=0&h=r0j8_L08luju5A-2EbFsB6CUznw5m6nfkN-cIoUiwlk=',
+//   'https://i.ytimg.com/vi/gdNLiiaNOdk/maxresdefault.jpg',
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Welsh_Dragon_%28Y_Ddraig_Goch%29.svg/1200px-Welsh_Dragon_%28Y_Ddraig_Goch%29.svg.png',
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/330px-Smaug_par_David_Demaret.jpg',
+//   'https://api.time.com/wp-content/uploads/2014/06/how-to-train-your-dragon-2-movie-still.jpg?quality=85&w=1200&h=628&crop=1',
+// ];
 
 /* Import App */
 import App from './components/App/App';
@@ -55,10 +55,13 @@ const getFavorite = function* (action) {
     // gets data from server
     const response = yield axios.get('/api/favorite');
 
+    console.log('response in favorite', response.data);
+
     yield put({
       type: 'SET_FAVORITE',
       payload: response.data,
     });
+
   } catch (err) {
     console.error(err);
   }
@@ -79,7 +82,8 @@ const sagaMiddleware = createSagaMiddleware();
 const favoriteGiphy = (state = [], action) => {
   switch (action.type) {
     case 'SET_FAVORITE':
-      return [...state, action.payload];
+      console.log('inside the reducer', action.payload);
+      return  action.payload;
     default:
       return state;
   }
