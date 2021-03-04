7698222df3afd4625cf110d7fa7f046a05a614b3
Johnshandsable <Jshands@asu.edu>
2021-03-04 11:00:50 -0600

    added a half working post server request

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -20,7 +20,19 @@ router.get('/', (req, res) => {
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  console.log('SERVER - POST inside /api/favorite');
+  const newImageURL = req.body.image_url;
+  console.log(req.body.image_url);
+  const sqlText = 'INSERT INTO "favorite_gifs" ("image_url") VALUES ($1)';
+  pool
+    .query(sqlText, [newImageURL])
+    .then((dbRes) => {
+      console.table(dbRes);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.error('SERVER - POST - an error occurred', err);
+    });
 });
 
 // update given favorite with a category id
