f2fdca15c0eff4123176f8025e1abc7d4552d4fb
Johnshandsable <Jshands@asu.edu>
2021-03-05 10:50:37 -0600

    added a favorite icon to buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ import DataGrid from '@material-ui/core';
 
 function App(props) {
   return (
-    <div className="margin-left">
+    <div className="margin-sides">
       <Router>
         <Nav />
 
--- src/components/SearchResultsItem/SearchResultsItem.jsx
+++ src/components/SearchResultsItem/SearchResultsItem.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 // MATERIAL UI
 import { Box, Grid, Button } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 /**
  *Function renders GIPHY image in search results list
@@ -23,11 +24,16 @@ function SearchResultsItem({ image }) {
 
   return (
     <Grid item xs={4}>
-      <Button color="primary" variant="outlined" onClick={addToFavorites}>
+      <Button
+        color="primary"
+        variant="outlined"
+        onClick={addToFavorites}
+        endIcon={<FavoriteIcon></FavoriteIcon>}
+      >
         Favorite
       </Button>
 
-      <Box mb={4} />
+      <Box mb={2} />
 
       <img
         className="search-result-image"
