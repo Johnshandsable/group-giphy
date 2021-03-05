e06b93442601e4d40d2da4093ba35c3aea9b77bd
Johnshandsable <Jshands@asu.edu>
2021-03-05 11:38:58 -0600

    gave more spacing to components

--- src/components/FavoriteViewItem/FavoriteViewItem.jsx
+++ src/components/FavoriteViewItem/FavoriteViewItem.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 // MATERIAL UI
-import { Box, Button, Grid } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  MenuList,
+  Select,
+} from '@material-ui/core';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 function FavoriteViewItem({ favorite }) {
@@ -32,7 +39,7 @@ function FavoriteViewItem({ favorite }) {
   };
 
   return (
-    <Grid item xs={4}>
+    <Grid item xs={6}>
       <Box mt={4} />
       <Button
         color="secondary"
@@ -44,21 +51,25 @@ function FavoriteViewItem({ favorite }) {
       </Button>
       <form onSubmit={setCategory}>
         {/* hard code values for basemode - refactor to generate options based on category list */}
-        <select
+        <Box mb={2} />
+        <Select
           name="category"
           defaultValue="Choose a category"
           onChange={(event) => setSelectedCategory(event.target.value)}
         >
-          <option value="Choose a category" disabled>
+          <MenuList value="Choose a category" disabled>
             Choose a category
-          </option>
-          <option value="1">Funny</option>
-          <option value="2">Cohort</option>
-          <option value="3">Cartoon</option>
-          <option value="4">NSFW</option>
-          <option value="5">Meme</option>
-        </select>
-        <input type="submit" value="Select Category" />
+          </MenuList>
+          <MenuItem value="1">Funny</MenuItem>
+          <MenuItem value="2">Cohort</MenuItem>
+          <MenuItem value="3">Cartoon</MenuItem>
+          <MenuItem value="4">NSFW</MenuItem>
+          <MenuItem value="5">Meme</MenuItem>
+        </Select>
+        <Box mb={2} />
+        <Button color="primary" variant="outlined" value="Select Category">
+          Submit
+        </Button>
       </form>
       <Box mb={2} />
       <img className="search-result-image" src={favorite.image_url} />
